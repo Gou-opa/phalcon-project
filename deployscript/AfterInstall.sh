f9c5fafb5d5c0b45e6705c14d36b791ff1e4cbac
Gou-Opa <monneymrt1998@gmail.com>
2019-01-29 12:02:13 +0700

    add code build file env

--- deployscript/AfterInstall.sh
+++ deployscript/AfterInstall.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
 echo done
+python3 convert.py
--- deployscript/BeforeInstall.sh
+++ deployscript/BeforeInstall.sh
@@ -4,3 +4,7 @@
 apt-get update
 #remove old deploy
 rm -rf /var/www/html
+apt-get install python3
+python-pip python-dev build-essential 
+pip install --upgrade pip 
+pip install --upgrade virtualenv 

d7f49e678dcfc824f7763e56270d8a46dc31eef5
Gou-Opa <monneymrt1998@gmail.com>
2019-01-29 12:46:57 +0700

    edit instal python

--- deployscript/BeforeInstall.sh
+++ deployscript/BeforeInstall.sh
@@ -4,7 +4,6 @@
 apt-get update
 #remove old deploy
 rm -rf /var/www/html
-apt-get install python3
-python-pip python-dev build-essential 
+apt-get install python3 python-pip python-dev build-essential 
 pip install --upgrade pip 
 pip install --upgrade virtualenv 

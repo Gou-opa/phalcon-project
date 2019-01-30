eda57838a2d6b6780207f00cbf67c2a06ff20fc5
Gou-Opa <monneymrt1998@gmail.com>
2019-01-30 13:44:05 +0700

    fix appspec

--- deployscript/BeforeInstall.sh
+++ deployscript/BeforeInstall.sh
@@ -4,6 +4,6 @@
 apt-get update
 #remove old deploy
 rm -rf /var/www/html
-apt-get install python3 python-pip python-dev build-essential 
+apt-get install python3 python-pip python-dev build-essential -y
 pip install --upgrade pip 
 pip install --upgrade virtualenv 

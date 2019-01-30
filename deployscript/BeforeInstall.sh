44affc8d65d4fb09276f78a6ca632007d366b8dc
Gou-Opa <monneymrt1998@gmail.com>
2019-01-30 17:42:03 +0700

    fix touch again

--- deployscript/BeforeInstall.sh
+++ deployscript/BeforeInstall.sh
@@ -6,5 +6,5 @@ apt-get update
 rm -rf /var/www/html
 apt-get install python3 python3-pip python3-dev build-essential -y
 mkdir -p /var/www/html
-touch /var/www/html.env
+touch /var/www/html/.env
 chown ubuntu:ubuntu /var/www/html/.env

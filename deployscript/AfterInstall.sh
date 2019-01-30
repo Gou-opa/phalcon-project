0de1c1977c0738fb58e12e3264c84580e69e12fc
Gou-Opa <monneymrt1998@gmail.com>
2019-01-30 17:37:39 +0700

    fix touch

--- deployscript/AfterInstall.sh
+++ deployscript/AfterInstall.sh
@@ -2,6 +2,5 @@
 
 pip3 install boto3
 pip3 install --upgrade pip
-touch .env
 echo done
 python3 /var/www/html/convert.py
--- deployscript/BeforeInstall.sh
+++ deployscript/BeforeInstall.sh
@@ -5,4 +5,6 @@ apt-get update
 #remove old deploy
 rm -rf /var/www/html
 apt-get install python3 python3-pip python3-dev build-essential -y
+mkdir -p /var/www/html
+touch /var/www/html.env
 chown ubuntu:ubuntu /var/www/html/.env

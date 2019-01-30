3252de4dd6133b799819f6329adebdff41a24eb8
Gou-Opa <monneymrt1998@gmail.com>
2019-01-30 15:47:43 +0700

    change own of .env for convert.py can open and wwrite to it

--- deployscript/AfterInstall.sh
+++ deployscript/AfterInstall.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 pip3 install boto3
-pip3 install pipupgrade
-pipupgrade --latest --yes
+pip3 install --upgrade pip
 echo done
 python3 /var/www/html/convert.py
--- deployscript/BeforeInstall.sh
+++ deployscript/BeforeInstall.sh
@@ -5,3 +5,4 @@ apt-get update
 #remove old deploy
 rm -rf /var/www/html
 apt-get install python3 python3-pip python3-dev build-essential -y
+chown ubuntu:ubuntu /var/www/html/.env

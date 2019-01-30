4a82f484da4da0550be4b58564d79f8af4d34a2d
Gou-Opa <monneymrt1998@gmail.com>
2019-01-30 16:42:55 +0700

    touch .env at build instead of deploy

--- deployscript/BeforeInstall.sh
+++ deployscript/BeforeInstall.sh
@@ -5,5 +5,4 @@ apt-get update
 #remove old deploy
 rm -rf /var/www/html
 apt-get install python3 python3-pip python3-dev build-essential -y
-> /var/www/html/.env
 chown ubuntu:ubuntu /var/www/html/.env
